edaeb2a77953d95ba11635130b1ca365b3ebfaa1
Kevin Lew <kalew5265@gmail.com>
2021-07-06 18:06:56 -0700

    Day 2 Daily Challenge completed

--- js/script.js
+++ js/script.js
@@ -10,3 +10,16 @@ const myName = 'Kevin';
 const favFood = 'likes garlic bread';
 
 console.log(myName, favFood);
+
+// Day 2 Take-home
+const superC = 'Supercalifragilisticexpialidocious';
+document.getElementById("numDemo").innerHTML = superC.length;
+console.log(superC.length);
+
+// Shows number in console along with what data type it is
+const numD2 = 57;
+console.log(typeof numD2, numD2);
+
+// Shows BigInt in console along with what data type it is
+const bigIntDemo = 1234567890123456789012345678901234567890n;
+console.log(typeof bigIntDemo, bigIntDemo)
