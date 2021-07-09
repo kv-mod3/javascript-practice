0de384356dbced414bee57ab8298112aab68a520
Kevin Lew <kalew5265@gmail.com>
2021-07-08 19:03:33 -0700

    updated day 3 take-home

--- js/day03.js
+++ js/day03.js
@@ -15,32 +15,46 @@ document.title = 'Day 03 - Take Home';
 const h1 = document.createElement('h1');
 h1.innerText = 'Day 03 - Take Home';
 const p = document.createElement('p');
-p.innerText = 'Please enter your first and lsst name, then click Confirm.';
+p.innerText = 'Please enter your first and last name, your birthday, and then click Confirm.';
 
 
 
 // Creates input field element and applies placeholder text for input field
 // Note: input fields output their values as strings
-let firstName = document.createElement('input');
+const br = document.createElement('br');
+
+const firstName = document.createElement('input');
 firstName.placeholder = 'First Name';
 
 // Creates input field element for last name
-let lastName = document.createElement('input');
+const lastName = document.createElement('input');
 lastName.placeholder = 'Last Name';
 
 // Creates Enter button
 const btn = document.createElement('button');
 btn.innerHTML = 'Confirm';
 
+// Create Birthday field
+const bDay = document.createElement('input');
+bDay.placeholder = 'Birthday';
+
 btn.addEventListener('click', function () {
     console.log('click');
 
     // accepts input
-    const fNameOutput = firstName.value;
-    const lNameOutput = lastName.value;
+    const fNamePrint = firstName.value;
+    const lNamePrint = lastName.value;
+    const bDayPrint = bDay.value;
+    const correctBDay = confirm(bDayPrint + '. Is this your correct birthday?');
+    if (correctBDay) {
+        alert(bDayPrint);
+    }
+    else {
+        alert('Incorrect birthday');
+    }
 
-    console.log(fNameOutput);
-    alert(lNameOutput);
+    console.log(`First Name: ${fNamePrint}`);
+    alert(`Your last name is ${lNamePrint}.`);
 });
 
 // Appends elements created above to the <body> section of the document
@@ -48,4 +62,6 @@ document.body.appendChild(h1);
 document.body.appendChild(p);
 document.body.appendChild(firstName);
 document.body.appendChild(lastName);
+document.body.appendChild(br);
+document.body.appendChild(bDay);
 document.body.appendChild(btn);
