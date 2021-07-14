ebd44113468c307fc514de1f2ae43998bf10bcc9
Kevin Lew <kalew5265@gmail.com>
2021-07-13 21:46:55 -0700

    added instructions for day 4 & 5

--- js/day04.js
+++ js/day04.js
@@ -1,7 +1,25 @@
+// ------------ DAILY CHALLENGE ------------
+// Prompt the user for two different numbers
+// Convert the values to integers
+// Store the two integers as variables
+// Compare the two numbers
+// Alert the larger number
+
+
+// ------------ TAKE HOME CHALLENGE ------------
+// Add an edge case to your daily challenge code
+// Check if the user input anything other than a number
+// If so, prompt the user again to input a number
+
+
+
 // Titles the document page, adds header + other info
 document.title = 'Day 04 - Take Home';
 const h1 = document.createElement('h1');
 h1.innerText = 'Day 04 - Take Home';
 document.body.appendChild(h1);
 
+
+
 // inputType Number
+// using a "while" loop solution may work for cleanliness
---
+++ js/day05.js
@@ -0,0 +1,20 @@
+/*
+DAILY CHALLENGE
+Write a function that accepts a number as an argument
+Alert the user whether the given number is even or odd
+Call your function
+
+
+TAKE HOME CHALLENGE
+Write a function that accepts a password (string)
+Validate the password using the following rules:
+    - Must be 6-20 characters long
+    - Must start with a letter
+Alert the user if their password has been accepted or why their password was rejected
+*/
+
+// Titles the document page, adds header + other info
+document.title = 'Day 05 - Take Home';
+const h1 = document.createElement('h1');
+h1.innerText = 'Day 05 - Take Home';
+document.body.appendChild(h1);
